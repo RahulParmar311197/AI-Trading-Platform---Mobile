77acbf8a9f9679b17b2a5cda9bc623284e02a8ea
RahulParmar311197 <mylearning311997@gmail.com>
2026-08-27 17:09:52 +0530

    fix(brokers): export paper broker adapter

--- backend/app/brokers/__init__.py
+++ backend/app/brokers/__init__.py
@@ -1,6 +1,7 @@
 """Broker adapters."""
 
 from app.brokers.base import BrokerAdapter
+from app.brokers.paper import PaperBrokerAdapter
 from app.brokers.upstox import UpstoxAdapter
 
-__all__ = ["BrokerAdapter", "UpstoxAdapter"]
+__all__ = ["BrokerAdapter", "PaperBrokerAdapter", "UpstoxAdapter"]
